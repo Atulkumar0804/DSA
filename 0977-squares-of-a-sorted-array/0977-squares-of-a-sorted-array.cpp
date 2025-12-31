bc30a5283807a1dfb32352c51a88b6f07bca9ac8
Atul Kumar Singh <atulkumarsingh0804@gmail.com>
2025-12-31 11:00:49 +0530

    Time: 0 ms (100%), Space: 30.4 MB (30.62%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,65 +1,28 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums)
-    {   vector<int>a; // negative array
-        vector<int>b; // positive array
-        for(int i=0;i<nums.size();i++) // split intital array into two
+    vector<int> sortedSquares(vector<int>& nums) 
+    {
+        int n=nums.size();
+        vector<int> res(n);
+        int left=0;
+        int right=n-1;
+        int pos=n-1;
+        while(left<=right)
         {
-            if(nums[i]<0)
-                a.push_back(nums[i]);
+            int leftsq=pow(nums[left],2);
+            int rightsq=pow(nums[right],2);
+            if(leftsq>rightsq)
+            {
+                res[pos]=leftsq;
+                left++;
+            }
             else
-                b.push_back(nums[i]);
+            {
+                res[pos]=rightsq;
+                right--;
+            }
+         pos--;
         }
-        if(a.size()==0) //no negative element in the initial array
-        {   
-            for(int i=0;i<nums.size();i++)
-            {
-                nums[i]=pow(nums[i],2);
-            }
-            return nums;   
-        }
-        else if(b.size()==0) // no positive element in initial array
-        {
-            for(int i=0;i<nums.size();i++)
-            {
-                nums[i]=pow(nums[i],2);
-            }
-            reverse(nums.begin(),nums.end()); // reverse the array for sorted array
-            return nums; 
-        }
-        else // both postive and negative elements in array
-        {
-            for(int i=0;i<a.size();i++)
-            {
-                a[i]=pow(a[i],2);//square negative elements
-            }
-            reverse(a.begin(),a.end());//reverse them
-            for(int i=0;i<b.size();i++)
-            {
-                b[i]=pow(b[i],2);//square positive elements
-            }
-            vector<int> result; // initialzed result array
-            int i=0,j=0;
-            while(i<a.size() && j<b.size())
-            {
-                if(a[i]<b[j])
-                {
-                    result.push_back(a[i]);
-                    i++;
-                }
-                else
-                {
-                    result.push_back(b[j]);
-                    j++;
-                }    
-            }
-            while(i<a.size())//only when above while is executed
-                result.push_back(a[i++]);
-            while(j<b.size())
-                result.push_back(b[j++]);
-        return result;// return final resultant array
-        }
-
-        
+    return res;
     }
 };
