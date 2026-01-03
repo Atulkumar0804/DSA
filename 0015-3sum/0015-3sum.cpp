6bc1d30f4f88b07cb55f41c7b988ad4d5929689f
Atul Kumar Singh <atulkumarsingh0804@gmail.com>
2026-01-03 12:09:56 +0530

    Time: 43 ms (83.86%), Space: 29.2 MB (45.62%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -24,7 +24,7 @@ public:
                     left++;
                     right--;
                 }
-                else if(sum<=target)
+                else if(sum<target)
                     left++;
                 else
                     right--;
